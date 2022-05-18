24d8c0a9d8f9a8ff6f9c7abdaea76570bfec43c6
Gian-Franco-L <GianXero@hotmail.com>
2022-05-17 22:36:40 -0300

    introAnimation

---
+++ script.js
@@ -0,0 +1,12 @@
+//let mainPanel = document.querySelectorA(".mainPanel")
+
+/*window.addEventListener("load", () =>{
+  mainPanel.classList.add("introAnimation")
+  setTimeout(() =>{
+    mainPanel.classList.add("outroAnimation")
+  }, 5000)
+  setTimeout(() =>{
+    mainPanel.classList.remove("introAnimation")
+    mainPanel.classList.remove("outroAnimation")
+  }, 7000)
+})*/
